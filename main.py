1a521b796254addc9b42bf73da49be94d908e671
unknown <montoyanicolas339@gmail.com>
2026-05-14 20:49:23 -0500

    feature main

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+from fastapi import FastAPI, Depends # <-- Asegúrate de importar Depends
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
 from db.database import create_db_and_tables
 from routers import proveedor, producto, categoria, cliente, venta, compra, estadisticas, usuario
-from db import models
 
-
+# Importamos tu función de seguridad (ajusta la ruta si tu archivo se llama diferente)
+from auth.auth import obtener_usuario_actual 
 
 app = FastAPI(title="API de Inventario")
-
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,18 +15,24 @@ app.add_middleware(
     allow_methods=["*"], 
     allow_headers=["*"],
 )
-#-------------------------------------------------------------------------------
 
+# -------------------------------------------------------------------------------
+# EL CANDADO MAESTRO: Exige un Token válido para dejar pasar la petición
+candado = [Depends(obtener_usuario_actual)]
 
-app.include_router(proveedor.router)
-app.include_router(producto.router)
-app.include_router(categoria.router)
-app.include_router(cliente.router)
-app.include_router(compra.router)
-app.include_router(venta.router)
-app.include_router(estadisticas.router)
+# Aplicamos el candado a todas las rutas que manejan información del negocio
+app.include_router(proveedor.router, dependencies=candado)
+app.include_router(producto.router, dependencies=candado)
+app.include_router(categoria.router, dependencies=candado)
+app.include_router(cliente.router, dependencies=candado)
+app.include_router(compra.router, dependencies=candado)
+app.include_router(venta.router, dependencies=candado)
+app.include_router(estadisticas.router, dependencies=candado)
+
+# ¡RUTAS PÚBLICAS! (Sin candado)
+# La ruta de usuarios DEBE ser pública para que procese el Login y devuelva el Token.
 app.include_router(usuario.router)
-
+# -------------------------------------------------------------------------------
 
 @app.on_event("startup")
 def on_startup():
