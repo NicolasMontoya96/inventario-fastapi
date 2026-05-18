04b1738eb5e1461ae8521bf1771663aae77040e4
unknown <montoyanicolas339@gmail.com>
2026-05-18 18:59:53 -0500

    fix: conexion base de datos produccion

--- db/database.py
+++ db/database.py
@@ -5,22 +5,28 @@ from dotenv import load_dotenv
 from sqlmodel import create_engine, SQLModel
 from db import models
 
-# Esto busca el archivo .env subiendo un nivel desde la carpeta 'db'
+
 base_dir = Path(__file__).resolve().parent.parent
 env_path = base_dir / ".env"
+
 
 load_dotenv(dotenv_path=env_path)
 
 database_url = os.getenv("DATABASE_URL")
 
 if not database_url:
-    raise ValueError(f"No se encontró el .env en: {env_path}")
+   
+    raise ValueError("¡Error CRÍTICO! No se encontró la variable DATABASE_URL en el entorno ni en el .env.")
+
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+# -------------------------------------------
 
 engine = create_engine(database_url)
 
 def create_db_and_tables():
-    # Asegúrate de importar tus modelos AQUÍ antes de llamar a create_all
-    # para que SQLModel sepa qué tablas crear.
+
     from db import models 
     SQLModel.metadata.create_all(engine)
 
