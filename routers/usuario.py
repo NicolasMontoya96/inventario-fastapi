91249833a2ad091cce203cf99460c69a23902d2b
unknown <montoyanicolas339@gmail.com>
2026-05-18 20:34:29 -0500

    chore: usuario

--- routers/usuario.py
+++ routers/usuario.py
@@ -11,9 +11,9 @@ router = APIRouter(
     prefix="/autenticacion",
     tags=["autenticacion"]
 )
-# ---------------------------------------------------------
-# 1. EL ENDPOINT PARA REGISTRAR AL USUARIO (Tu papá, tú, etc.)
-# ---------------------------------------------------------
+
+
+'''
 @router.post("/registro", response_model=UsuarioResponse, status_code=status.HTTP_201_CREATED)
 def registrar_usuario(usuario: UsuarioCreate, session: Session = Depends(get_session)):
     
@@ -22,10 +22,10 @@ def registrar_usuario(usuario: UsuarioCreate, session: Session = Depends(get_ses
     if usuario_db:
         raise HTTPException(status_code=400, detail="El nombre de usuario ya está en uso")
     
-    # ¡LA MAGIA AQUÍ! Encriptamos la contraseña plana antes de guardarla
+   
     password_encriptada = obtener_password_hash(usuario.password)
     
-    # Creamos el usuario con el hash, NUNCA con la contraseña real
+    
     nuevo_usuario = Usuario(
         username=usuario.username,
         password_hash=password_encriptada,
@@ -36,21 +36,19 @@ def registrar_usuario(usuario: UsuarioCreate, session: Session = Depends(get_ses
     session.commit()
     session.refresh(nuevo_usuario)
     return nuevo_usuario
+'''
 
 
-# ---------------------------------------------------------
-# 2. EL ENDPOINT DE LOGIN (La taquilla para pedir el Token)
-# ---------------------------------------------------------
 @router.post("/login", response_model=Token)
 def login_para_obtener_token(
-    # OAuth2PasswordRequestForm es especial: lee los datos como formulario, no como JSON
+    
     form_data: OAuth2PasswordRequestForm = Depends(), 
     session: Session = Depends(get_session)
 ):
     # 1. Buscamos al usuario en la base de datos
     usuario = session.exec(select(Usuario).where(Usuario.username == form_data.username)).first()
     
-    # 2. Si no existe o la contraseña no coincide, lanzamos error genérico (por seguridad)
+    # 2. Si no existe o la contraseña no coincide, lanzamos error genérico
     if not usuario or not verificar_password(form_data.password, usuario.password_hash):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
