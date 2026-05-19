7026129d23e8441cbf557f1b871246f70fa2b0f8
unknown <montoyanicolas339@gmail.com>
2026-05-18 20:01:23 -0500

    chore: agregar url de netlify al CORS

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-from fastapi import FastAPI, Depends # <-- Asegúrate de importar Depends
+from fastapi import FastAPI, Depends 
 from fastapi.middleware.cors import CORSMiddleware
 from db.database import create_db_and_tables
 from routers import proveedor, producto, categoria, cliente, venta, compra, estadisticas, usuario
 
-# Importamos tu función de seguridad (ajusta la ruta si tu archivo se llama diferente)
+
 from auth.auth import obtener_usuario_actual 
 
 app = FastAPI(title="API de Inventario")
 
-# 1. Creamos una "Lista Blanca" con la URL de tu Frontend
+
 origenes_permitidos = [
-    "http://localhost:5173",     # Puerto estándar de Vite
-    "http://127.0.0.1:5173",     # Variante por si usas la IP directa
+    "http://localhost:5173",                     
+    "http://127.0.0.1:5173",                     
+    "https://jazzy-entremet-597d36.netlify.app" 
 ]
 
 # 2. Aplicamos la lista al CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origenes_permitidos,  # <--- Quitamos el "*" y ponemos la lista
+    allow_origins=origenes_permitidos,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# -------------------------------------------------------------------------------
-# EL CANDADO MAESTRO: Exige un Token válido para dejar pasar la petición
+
 candado = [Depends(obtener_usuario_actual)]
 
-# Aplicamos el candado a todas las rutas que manejan información del negocio
+
 app.include_router(proveedor.router, dependencies=candado)
 app.include_router(producto.router, dependencies=candado)
 app.include_router(categoria.router, dependencies=candado)
@@ -36,8 +36,7 @@ app.include_router(compra.router, dependencies=candado)
 app.include_router(venta.router, dependencies=candado)
 app.include_router(estadisticas.router, dependencies=candado)
 
-# ¡RUTAS PÚBLICAS! (Sin candado)
-# La ruta de usuarios DEBE ser pública para que procese el Login y devuelva el Token.
+
 app.include_router(usuario.router)
 # -------------------------------------------------------------------------------
 
